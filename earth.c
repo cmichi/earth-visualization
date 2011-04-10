3a4ac7efdc4bc40be149e2da831a083f28e88dbf
michm <mich@elmueller.net>
2011-04-10 14:32:29 +0200

    added copyright notice

--- earth.c
+++ earth.c
@@ -1,6 +1,10 @@
 /* 
  * Copyright (c) 2011, Michael Müller <http://micha.elmueller.net>
  *
+ * Blueprint code taken from
+ * xscreensaver, Copyright(c) 2001-2008 Jamie Zawinski <jwz@jwz.org>
+ *
+ *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
  * the above copyright notice appear in all copies and that both that
